845f25a690d9888b3f96fdf0858848098d8ab001
Carlos Daniel <carlosd.1199@gmail.com>
2020-12-19 18:57:22 -0300

    Adds register user endpoint #3

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 
 urlpatterns = [
+  
+  path('register/', UserRegisterView.as_view({'post': 'create'})),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+class UserRegisterView(ModelViewSet):
+	parser_classes = (JSONParser,)
+	serializer_class = UserSerializer
+	permission_classes = [AllowAny]
+
+	def create(self, request):
+		serializer = self.serializer_class(data=request.data)
+		
+		status_code = status.HTTP_201_CREATED
+		response_data = {}
+
+		if not serializer.is_valid():
+			response_data = serializer.errors
+			status_code = status.HTTP_400_BAD_REQUEST
+		else:
+			serializer.save()
+			response_data = serializer.data
+
+		return Response(data=response_data, status=status_code)
